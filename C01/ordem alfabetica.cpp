2982cbf433d722ace405eb4d18c0b3e8a4c31f80
Jhone93567 <joaogabrielchereze@hotmail.com>
2023-05-18 15:33:56 -0300

    a

--- C01/ordem alfabetica.cpp
+++ C01/ordem alfabetica.cpp
@@ -5,7 +5,39 @@ using namespace std;
 
 int main()
 {
-    
+    // 3 strangs, palavra 1 2 e 3
+    char p1[100];
+    char p2[100];
+    char p3[100];
 
+    cin.getline(p1,100);
+    cin.getline(p2,100);
+    cin.getline(p3,100);
+
+    if (strcmp(p1,p2) == -1 && strcmp(p2,p3) == -1 && strcmp(p1,p3) == -1)
+    {
+        cout << p1 << " " << p2 << " " << p3 << endl;
+    }
+    else if (strcmp(p1,p2) == -1 && strcmp(p2,p3) == +1 && strcmp(p1,p3) == -1)
+    {
+        cout << p1 << " " << p3 << " " << p2 << endl;
+    }
+    else if (strcmp(p1,p2) == +1 && strcmp(p2,p3) == -1 && strcmp(p1,p3) == -1)
+    {
+        cout << p2 << " " << p1 << " " << p3 << endl;
+    }
+    else if (strcmp(p1,p2) == +1 && strcmp(p2,p3) == -1 && strcmp(p1,p3) == +1)
+    {
+        cout << p2 << " " << p3 << " " << p1 << endl;
+    }
+    else if (strcmp(p1,p2) == -1 && strcmp(p2,p3) == +1 && strcmp(p1,p3) == +1)
+    {
+        cout << p3 << " " << p1 << " " << p2 << endl;
+    }
+    else if (strcmp(p1,p2) == +1 && strcmp(p2,p3) == +1 && strcmp(p1,p3) == +1)
+    {
+        cout << p3 << " " << p2 << " " << p1 << endl;
+    }
+    
     return 0;
 }
