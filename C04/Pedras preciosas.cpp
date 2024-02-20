d25d2c432583013d2a70950fde3716cec6a27ec3
Jhone93567 <joaogabrielchereze@hotmail.com>
2024-02-20 14:51:32 -0300

    comeco do codigo do dia 20 fev

---
+++ C04/Pedras preciosas.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+
+using namespace std;
+
+struct pedra
+{
+    int volume;
+    int peso;
+    int valor;
+};
+
+int main()
+{
+    pedra mina[10];
+    mina[0].volume = 455;
+    
+
+
+}
