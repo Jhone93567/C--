0d4fba78a52f2151324df4c71445fe12ac43bb53
Jhone93567 <joaogabrielchereze@hotmail.com>
2024-03-27 22:44:48 -0300

    Lista de adjacencias

--- C04/GR - Lista de Adjacencias.cpp
+++ C04/GR - Lista de Adjacencias.cpp
@@ -11,12 +11,21 @@ struct no
 
 void cria_aresta(list<no> adj[], int u, int v, int p, int orientado)
 {
-    if (orientado == 1)
+    no aux; // auxiliar do tipo da struct para armazenar os dados que serao inseridos na lista
+    aux.v = v; // armazenando v na auxiliar
+    aux.peso = p; // armazenando o peso
+
+    if (orientado == 1) // caso seja um grafo orientado
     {
-        
+        adj[u].push_back(aux); // apenas insere o vertice e seu peso
     }
-    else
+    else // caso seja um grafo nao ordenado
     {
+        adj[u].push_back(aux); // insere o vertice e peso
+
+        // inverte o destino e o vertice que sera inserido
+        aux.v=u; 
+        adj[v].push_back(aux);   
     }
 }
 
@@ -25,16 +34,28 @@ int main()
     int vertices, origem, destino, peso;
     int orientado;
 
-    cin >> vertices >> orientado;
+    cin >> vertices >> orientado; // Entrada do numero de vertices e se o grafo e orientado ou nao
 
-    list<no> adj[10];
+    list<no> adj[10]; // declarando a lista de adjacencia
+    list<no>::iterator it; // declarando um iterator para ajudar na saida
 
-    cin >> origem >> destino >> peso;
+    cin >> origem >> destino >> peso; // entrada dos dados a serem inseridos
 
-    while (origem != -1 && destino != -1 && peso != -1)
+    while (origem != -1 && destino != -1 && peso != -1) // enquanto nao receber todos os parametros -1
     {
-        cria_aresta(adj, origem, destino, peso, orientado);
+        cria_aresta(adj, origem, destino, peso, orientado); // cadastra o vertice no grafo
+
+        cin >> origem >> destino >> peso; // entrada dos parametros dnv
     }
+
+    for (int i = 0; i < 10; i++) // le o grafo todo
+    {
+        for (it=adj[i].begin();it != adj[i].end();it++) // le cada elemento do vetor
+        {
+            cout << i << " " << it->v << " " << it->peso << endl; // Imprime na ordem, vertice, destino e peso
+        }   
+    }
+    
 
     return 0;
 }
